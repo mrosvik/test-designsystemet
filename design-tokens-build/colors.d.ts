62872184fc13899817739944fa57eec954097c85
Marianne Røsvik <marianne.rosvik@digdir.no>
2025-03-03 08:56:04 +0100

    variables

---
+++ design-tokens-build/colors.d.ts
@@ -0,0 +1,16 @@
+import type {} from '@digdir/designsystemet-react/colors';
+
+declare module '@digdir/designsystemet-react/colors' {
+  export interface MainAndSupportColors {
+    bo1: never;
+    bo2: never;
+    bo3: never;
+    bo4: never;
+    bo5: never;
+    ex1: never;
+    ex2: never;
+    ex3: never;
+    ex4: never;
+    ex5: never;
+  }
+}
